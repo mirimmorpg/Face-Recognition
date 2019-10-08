ff033027538e03618ceaffddeab3a2ef4f6b5e49
Miriam Tanski <miriam.tanski@jvm.de>
2019-10-08 14:47:32 +0200

    :fire: remove test pages

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
-const imageUpload = document.getElementById("imageUpload");
+const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models")
-]).then(start);
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo);
 
-async function start() {
-  const container = document.createElement("div");
-  container.style.position = "relative";
-  document.body.append(container);
+async function startVideo() {
   const labeledFaceDescriptors = await loadLabeledImages();
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-  document.body.append("Loaded");
-  imageUpload.addEventListener("change", async () => {
-    const image = await faceapi.bufferToImage(imageUpload.files[0]);
-    document.body.append(image);
-    const canvas = faceapi.createCanvasFromMedia(image);
-    container.append(canvas);
-    const displaySize = { width: image.width, height: image.height };
+
+  navigator.getUserMedia(
+    {
+      video: {}
+    },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+
+  video.addEventListener("play", () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height };
     faceapi.matchDimensions(canvas, displaySize);
-    const detections = await faceapi
-      .detectAllFaces(image)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    const results = resizedDetections.map(d =>
-      faceMatcher.findBestMatch(d.descriptor)
-    );
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: result.toString()
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions()
+        .withFaceDescriptors();
+
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      const results = resizedDetections.map(d =>
+        faceMatcher.findBestMatch(d.descriptor)
+      );
+      results.forEach((result, i) => {
+        const box = resizedDetections[i].detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: result.toString()
+        });
+        drawBox.draw(canvas);
       });
-      drawBox.draw(canvas);
-    });
+    }, 100);
   });
 }
-
 function loadLabeledImages() {
   const labels = [
     "Black Widow",
     "Captain America",
     "Captain Marvel",
+    "Dennis",
     "Hawkeye",
     "Jim Rhodes",
     "Thor",
-    "Tony Stark"
+    "Tony Stark",
+    "Miriam",
+    "Leo",
+    "Beyonce",
+    "Rihanna"
   ];
   return Promise.all(
     labels.map(async label => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
         const img = await faceapi.fetchImage(
-          `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+          `./labeled_images/${label}/${i}.jpg`
         );
         const detections = await faceapi
           .detectSingleFace(img)
