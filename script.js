3be2040e81ab03ff77553cf7431f8327d57bdca5
Miriam Tanski <miriam.tanski@jvm.de>
2019-10-08 15:08:21 +0200

    add ssd-model

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
   faceapi.nets.faceExpressionNet.loadFromUri("/models")
 ]).then(startVideo);
 
